da4da73a655216dbec8240d1407ac9f48982a179
Buğrahan Çoban <149260654+Bugrahancobain@users.noreply.github.com>
2024-12-29 01:41:21 +0300

    Update layout.js

--- app/[locale]/layout.js
+++ app/[locale]/layout.js
@@ -29,7 +29,6 @@ export default async function RootLayout({ children, params }) {
   return (
     <html lang={locale}>
       <head>
-    <script type="text/javascript" data-cmp-ab="1" src="https://cdn.consentmanager.net/delivery/autoblocking/ea6d55d0c2402.js" data-cmp-host="b.delivery.consentmanager.net" data-cmp-cdn="cdn.consentmanager.net" data-cmp-codesrc="16"></script>
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
@@ -37,6 +36,7 @@ export default async function RootLayout({ children, params }) {
           href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900&family=Quicksand:wght@300..700&display=swap"
           rel="stylesheet"
         />
+            <script type="text/javascript" data-cmp-ab="1" src="https://cdn.consentmanager.net/delivery/autoblocking/ea6d55d0c2402.js" data-cmp-host="b.delivery.consentmanager.net" data-cmp-cdn="cdn.consentmanager.net" data-cmp-codesrc="16"></script>
         {/* Google Analytics */}
         <script
           async
